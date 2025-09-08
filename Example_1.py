a3977343588e16001a7b307914f1fee84c985d2b
triniax2 <154000393+triniax2@users.noreply.github.com>
2025-09-08 10:07:12 -0400

    Example_1.py

--- Example_1.py
+++ Example_1.py
@@ -6,7 +6,10 @@ def equation_1(x):
     return 4 * x + 2
 
 def equation_2(x):
-    return 2 * x
+    return 2 * x + 0
+
+def equation_3(x):
+    return 2 * x + 2
 
 
 # To generate x values
@@ -16,20 +19,24 @@ x = np.linspace(-10, 10, 100)
 # To calculate "y" values for both equations
 y1 = equation_1(x)
 y2 = equation_2(x)
+y3 = equation_3(x)
+
 
 # Creating the plot
 plt.figure(figsize=(8, 6))
 
 # Plotting the two equations
-plt.plot(x, y1, label=r'$y = 4x + 2$', color='blue', linewidth=2)
-plt.plot(x, y2, label=r'$yr = 2x$', color='green', linewidth=2)
+plt.plot(x, y1, label=r'$y(x) = 4x + 2$', color='blue', linewidth=2)
+plt.plot(x, y2, label=r'$yr1(x) = 2x + 0$', color='red', linewidth=2)
+plt.plot(x, y3, label=r'$yr2(x) = 2x + 2$', color='green', linewidth=2)
+
 
 # Adding labels and title
 plt.xlabel('x')
 plt.ylabel('y')
 
 # plt.title('Plot of Two Linear Equations')
-plt.ylim(-40, 40)
+plt.ylim(-50, 50)
 
 # Adding a legend
 plt.legend()
