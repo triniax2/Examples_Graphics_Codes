a536853b594d5f5dc1800fd994e874c499c552cc
triniax2 <154000393+triniax2@users.noreply.github.com>
2025-09-25 00:33:38 -0400

    Example_2.py

--- Example_2.py
+++ Example_2.py
@@ -8,12 +8,14 @@ x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
 y_sin = np.sin(x)
 y_r1 = np.sin(x)*0.5 + 0.5
 y_r2 = np.sin(x)*0.5 - 0.5
+y_r3 = np.sin(x)*0.5 + 0
 
 # Creating the plot
 plt.figure(figsize=(10, 6))
 plt.plot(x, y_sin, label='sin(x)', color='blue')
 plt.plot(x, y_r1, label='sin(x) + 0.5', color='red')
 plt.plot(x, y_r2, label='sin(x) - 0.5', color='green')
+plt.plot(x, y_r3, label='0.5*sin(x) + 0', color='orange')
 
 # labels and lims
 plt.xlabel('x')
