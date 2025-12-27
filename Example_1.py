10d0d1e2d8d48777d647187f3970a09e41adfc4f
triniax2 <154000393+triniax2@users.noreply.github.com>
2025-12-26 21:37:50 -0400

    Update Example_1.py

--- Example_1.py
+++ Example_1.py
@@ -3,14 +3,10 @@ import matplotlib.pyplot as plt
 
 # Two define for the two linear equations
 def equation_1(x):
-    return 4 * x + 2
+    return 3 * x + 6
 
 def equation_2(x):
-    return 2 * x + 0
-
-def equation_3(x):
-    return 2 * x + 2
-
+    return 6 * x + 0
 
 # To generate x values
 x = np.linspace(-10, 10, 100)
@@ -19,23 +15,23 @@ x = np.linspace(-10, 10, 100)
 # To calculate "y" values for both equations
 y1 = equation_1(x)
 y2 = equation_2(x)
-y3 = equation_3(x)
 
 
 # Creating the plot
 plt.figure(figsize=(8, 6))
 
+
 # Plotting the two equations
-plt.plot(x, y1, label=r'$y(x) = 4x + 2$', color='blue', linewidth=2)
-plt.plot(x, y2, label=r'$yr1(x) = 2x + 0$', color='red', linewidth=2)
-plt.plot(x, y3, label=r'$yr2(x) = 2x + 2$', color='green', linewidth=2)
+plt.plot(x, y1, label=r'$y(x) = 3x + 6$', color='blue', linewidth=2)
+plt.plot(x, y2, label=r'$yr(x) = 6x + 0$', color='orange', linewidth=2)
 
 
-# Adding labels and lim
+# Adding labels 
 plt.xlabel('x')
-plt.ylabel('y')
+plt.ylabel('y(x)')
 
-plt.ylim(-50, 50)
+# plt y-axis range
+plt.ylim(-100, 100)
 
 # Adding a legend
 plt.legend()
